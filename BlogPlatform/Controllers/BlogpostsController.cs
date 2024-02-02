6fc859f3cc8b39f27259425e2c31cf02ce518b86
abc <abc@gmail.com>
2024-02-02 22:51:27 +0530

    Add project files.

---
+++ BlogPlatform/Controllers/BlogpostsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlogPlatform.Data;
+using BlogPlatform.Models;
+
+namespace BlogPlatform.Controllers
+{
+    public class BlogpostsController : Controller
+    {
+        private BloggingDbContext db = new BloggingDbContext();
+
+        // GET: Blogposts
+        public ActionResult Index()
+        {
+            return View(db.Blogposts.ToList());
+        }
+
+        // GET: Blogposts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blogpost blogpost = db.Blogposts.Find(id);
+            if (blogpost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogpost);
+        }
+
+        // GET: Blogposts/Create
+        public ActionResult Create()
+        {
+            return View(new Blogpost());
+        }
+
+        // POST: Blogposts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Content,PublicationDate,Author")] Blogpost blogpost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Blogposts.Add(blogpost);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blogpost);
+        }
+
+        // GET: Blogposts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blogpost blogpost = db.Blogposts.Find(id);
+            if (blogpost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogpost);
+        }
+
+        // POST: Blogposts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Content,PublicationDate,Author")] Blogpost blogpost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blogpost).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blogpost);
+        }
+
+        // GET: Blogposts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blogpost blogpost = db.Blogposts.Find(id);
+            if (blogpost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogpost);
+        }
+
+        // POST: Blogposts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Blogpost blogpost = db.Blogposts.Find(id);
+            db.Blogposts.Remove(blogpost);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
